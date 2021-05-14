c5785eed536453571c4921a198589bcae33db8cc
Bruno de Souza Disliler <bruno_disliler@outlook.com>
2021-05-14 19:04:52 -0300

    Project 001 - Name Band Generator

---
+++ 100 Projects/001 - Name Band Generator.py
@@ -0,0 +1,4 @@
+print('Welcome to the band name Generator!')
+city = input('What´s name of the city you grew up?\n')
+pet = input('What´s your pet´s name?\n')
+print(f'Your band name could be {city} {pet}')
