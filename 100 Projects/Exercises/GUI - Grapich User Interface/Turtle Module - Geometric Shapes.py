6582d68084ed1533d9cdd949bf14e7ffa7e9c935
Bruno de Souza Disliler <bruno_disliler@outlook.com>
2021-05-28 23:33:06 -0300

    GUI Projects

---
+++ 100 Projects/Exercises/GUI - Grapich User Interface/Turtle Module - Geometric Shapes.py
@@ -0,0 +1,22 @@
+import turtle as t
+import random
+
+tin = t.Turtle()
+
+colours = ["dark slate gray", "sandy brown", "dark red", "indian red",
+           "medium violet red", "blue violet", "blue", "dim gray"]
+
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tin.forward(100)
+        tin.right(angle)
+
+
+for shape_side_n in range(3, 11):
+    tin.color(random.choice(colours))
+    draw_shape(shape_side_n)
+
+screen = t.Screen()
+screen.exitonclick()
---
+++ 100 Projects/Exercises/GUI - Grapich User Interface/Turtle Module - Random Walk.py
@@ -0,0 +1,28 @@
+import turtle as t
+import random
+
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    new_color = (r, g, b)
+    return new_color
+
+
+tom = t.Turtle()
+tom.shape('classic')
+tom.width(15)
+tom.speed('fastest')
+directions = [0, 90, 180, 270]
+
+
+for _ in range(200):
+    tom.color(random_color())
+    tom.forward(30)
+    tom.setheading(random.choice(directions))
+
+screen = t.Screen()
+screen.exitonclick()
